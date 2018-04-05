2bb62c6c8a8cc908d9c352fb3b140fa95005e86d
OctoGanger <lannda.lathur@gmail.com>
2018-04-05 14:17:12 -0500

    Delete nitrogeno.java

--- src/com/company/nitrogeno.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class nitrogeno extends Congelados{
-
-    public nitrogeno(String caducidad,int lote, int temperatura){
-        super(caducidad,lote,temperatura);
-    }
-
-}
